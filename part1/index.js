e338db236d99bc816782ddc333fd6c4826c116f4
vikas <vkassharma19492@gmail.com>
2023-02-09 16:59:51 +0530

    updating backend with get, post and delete urls handle with event handler urls

--- part1/index.js
+++ part1/index.js
@@ -1,7 +1,8 @@
+const { json } = require('express')
 const express = require('express')
 const app = express()
 
-const http = require('http')
+app.use(express.json())
 
 let notes = [
     {
@@ -38,6 +39,38 @@ app.get('/api/notes/:id', (request, response) => {
     } else {
         response.status(404).end()
     }
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    notes = notes.filter(note => note.id !== id)
+    response.status(204).end()
+})
+
+const generateId = () => {
+    const maxId = notes.length > 0
+        ? Math.max(...notes.map(n => n.id))
+        : 0
+    return maxId + 1
+}
+
+app.post('/api/notes', (request, response) => {
+    const body = request.body
+    if (!body.content) {
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    const note = {
+        content: body.content,
+        important: body.important || false,
+        date: new Date(),
+        id: generateId(),
+    }
+
+    notes = notes.concat(note)
+    response.json(note)
 })
 
 const PORT = 3001
