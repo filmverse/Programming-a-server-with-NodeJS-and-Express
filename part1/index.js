270a461e830e4739f429b9513256c708ffe7db2b
vikas <vkassharma19492@gmail.com>
2023-02-12 00:12:03 +0530

    notes react app is now connected with notes express server

--- part1/index.js
+++ part1/index.js
@@ -1,8 +1,10 @@
 const { json } = require('express')
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
 app.use(express.json())
+app.use(cors())
 
 let notes = [
     {
--- part3/src/App.js
+++ part3/src/App.js
@@ -1,5 +1,19 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const App = () => {
+
+  const [ notes, setNotes ] = useState([])
+
+  const hook = () => {
+    axios.get('http://localhost:3001/api/notes').then(
+      note => {
+        console.log(note.data)
+      }
+    )
+  }
+  useEffect(hook, [])
+
   return (
     <div></div>
   )
