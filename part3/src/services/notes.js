bb9b6bee795d594558562d070b0066ea7d7fc6df
vikas <vkassharma19492@gmail.com>
2023-02-14 02:07:07 +0530

    notes react app is now connected with notes express server

--- part3/src/services/notes.js
+++ part3/src/services/notes.js
@@ -1,16 +1,16 @@
 import axios from "axios";
-const baseUrl = 'http://localhost:3001/api/notes'
+const baseUrl = '/api/notes'
 
 const getAll = () => {
     const request = axios.get(baseUrl)
-    // return request.then(response => response.data)
-    const nonExisting = {
-        id: 1000,
-        content: 'This note is not saved to the server',
-        date: '2019-05-30T17:30:31.098Z',
-        important: true,
-    }
-    return request.then(response => response.data.concat(nonExisting))
+    return request.then(response => response.data)
+    // const nonExisting = {
+    //     id: 1000,
+    //     content: 'This note is not saved to the server',
+    //     date: '2019-05-30T17:30:31.098Z',
+    //     important: true,
+    // }
+    // return request.then(response => response.data.concat(nonExisting))
 }
 
 const create = (nObject) => {
