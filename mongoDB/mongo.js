43109d0d9304051e2fb04a48fdefefad733314ce
vikas <vkassharma19492@gmail.com>
2023-02-20 18:36:26 +0530

    mongoDB adding data to the database & fetching data from the database

--- mongoDB/mongo.js
+++ mongoDB/mongo.js
@@ -20,12 +20,22 @@ const noteSchema = new mongoose.Schema({
 
 const Note = mongoose.model('Note', noteSchema)
 
-const note = new Note({
-    content: 'HTML is Easy',
-    important: true,
-})
+// Generating data for the database
 
-note.save().then(result => {
-    console.log('note saved!')
+// const note = new Note({
+//     content: 'HTML is Easy',
+//     important: true,
+// })
+
+// note.save().then(result => {
+//     console.log('note saved!')
+//     mongoose.connection.close()
+// })
+
+// fetching data from the database
+Note.find({}).then(result => {
+    result.forEach(note => {
+        console.log(note)
+    })
     mongoose.connection.close()
 })
