9c9ac2751b29c70f32e89571f5f8f643323cf6f6
vikas <vkassharma19492@gmail.com>
2023-02-25 02:05:59 +0530

    deployNotes

--- part6/index.js
+++ part6/index.js
@@ -3,6 +3,7 @@ const app = express()
 const cors = require('cors')
 app.use(express.json())
 app.use(cors())
+app.use(express.static('build'))
 
 let notes = [
     {
@@ -69,7 +70,7 @@ app.delete('/api/notes/:id', (request, response) => {
     response.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on part ${PORT}`)
 })
